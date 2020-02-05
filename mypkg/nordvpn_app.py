8a023b0bc39065c9af584fe8eb9ef8cc31c29ec0
dimitris <mitsos-r21@hotmail.gr>
2020-02-05 23:04:43 +0100

    Show main window when "Login" is clicked.
    Check when opening the application if the user is already logged in in the CLI tool

--- mypkg/nordvpn_app.py
+++ mypkg/nordvpn_app.py
@@ -1,41 +1,20 @@
 import sys
-import time
 
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 
 from mypkg.linux_cli import LinuxCli
 from mypkg.login_view import LoginUi
 from mypkg.main_view import MainUi
 from mypkg.nord_api import NordApi
 
-
-class LoginWindow(QtWidgets.QMainWindow, LoginUi):
-    def __init__(self, *args, obj=None, **kwargs):
-        super(LoginWindow, self).__init__(*args, **kwargs)
-        self.setupUi(self)
-        self.cli = LinuxCli()
-        if self.cli.isUserLoggedIn():
-            self.repaint()
-            time.sleep(0.5)
-            self.hide()
-            MainWindow().show()
-
-        self.login_button.clicked.connect(lambda: self.login())
-
-    def login(self):
-        if self.cli.login('r21jim@gmail.com', 'dvN9P2k52MmB'):
-            self.repaint()
-            time.sleep(0.5)
-            self.hide()
-            MainWindow().show()
-
+cli = LinuxCli()
+api = NordApi()
 
 
 class MainWindow(QtWidgets.QMainWindow, MainUi):
     def __init__(self, *args, obj=None, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
-        self.api = NordApi()
         self.data = None
 
         # initialize data
@@ -43,13 +22,13 @@ class MainWindow(QtWidgets.QMainWindow, MainUi):
         self.populate_country_list()
 
     def get_data(self):
-        self.data = self.api.get_api_data()
+        self.data = api.get_api_data()
 
     def refresh(self):
         self.get_data()
 
     def populate_country_list(self):
-        self.countries_list.addItems(self.api.get_country_list(self.data))
+        self.countries_list.addItems(api.get_country_list(self.data))
         self.countries_list.itemClicked.connect(lambda: self.populate_server_list())
 
     def populate_server_list(self):
@@ -57,7 +36,7 @@ class MainWindow(QtWidgets.QMainWindow, MainUi):
         current_country = self.countries_list.currentItem().text()
         server_list = []
         if current_country is not None:
-            server_list = self.api.get_servers_by_country(self.data, current_country)
+            server_list = api.get_servers_by_country(self.data, current_country)
         formatted_server_list = []
         for server in server_list:
             formatted_server_list.append(self.format_server_list_item(server))
@@ -71,8 +50,46 @@ class MainWindow(QtWidgets.QMainWindow, MainUi):
         return name + '\n' + 'Load: ' + str(load) + '%\n' + 'Domain: ' + domain
 
 
-app = QtWidgets.QApplication(sys.argv)
+class LoginWindow(QtWidgets.QMainWindow, LoginUi):
+    switch_window = QtCore.pyqtSignal()
 
-window = LoginWindow()
-window.show()
-app.exec()
+    def __init__(self, *args, obj=None, **kwargs):
+        super(LoginWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+
+        self.login_button.clicked.connect(lambda: self.login())
+
+    def login(self):
+        self.switch_window.emit()
+
+
+class Controller:
+
+    def __init__(self):
+        self.window = MainWindow()
+        self.login = LoginWindow()
+
+    def show_login(self):
+        self.login.switch_window.connect(self.show_main)
+        self.login.show()
+
+    def show_main(self):
+        self.login.close()
+        self.window.show()
+
+    def show_starting_window(self):
+        if cli.isUserLoggedIn():
+            self.show_main()
+        else:
+            self.show_login()
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    controller = Controller()
+    controller.show_starting_window()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
